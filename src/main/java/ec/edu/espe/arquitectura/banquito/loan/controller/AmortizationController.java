7684252bc617671205ab2d6520775f6cfe40797f
Daniel Alonso Gavilanes Falconi <dagavilanes2@espe.edu.ec>
2023-08-31 20:21:03 -0500

    update

--- src/main/java/ec/edu/espe/arquitectura/banquito/loan/controller/AmortizationController.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/loan/controller/AmortizationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/amortization")
+@RequestMapping("api/v1/amortization")
 public class AmortizationController {
 
     private final AmortizationService amortizationService;
